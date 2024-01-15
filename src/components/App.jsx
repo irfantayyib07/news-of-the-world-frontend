e9a42e3452077d04c2903ce735635df2490bf3c0
Irfan <irfantayyib07@gmail.com>
2024-01-15 21:38:00 +0500

    ns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '../App.css';
 import NavBar from './NavBar';
 import News from './News';
 
