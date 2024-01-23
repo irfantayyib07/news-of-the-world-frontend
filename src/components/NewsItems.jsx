d4c1443b59e4a61c61a6acf07e8e73113f8acd9d
Irfan <irfantayyib07@gmail.com>
2024-01-23 14:46:55 +0500

    deleted readme.md

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -4,11 +4,11 @@ function NewsItems({ title, description, imgUrl, newsUrl }) {
  return (
   <>
    <div className="card h-100">
-    <img src={!imgUrl ? "https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3c3xlbnwwfHwwfHx8MA%3D%3D" : imgUrl} className="card-img-top card-image" alt={"No Image Found"}/>
+    <img src={!imgUrl ? "https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3c3xlbnwwfHwwfHx8MA%3D%3D" : imgUrl} className="card-img-top card-image" alt={"No Image Found"} />
     <div className="card-body d-flex flex-column">
      <h5 className="card-title">{title}...</h5>
      <p className="card-text">{description}...</p>
-     <a href={newsUrl} target='_blank' className="btn btn-primary mt-auto d-block" style={{width: "fit-content"}}>Read more</a>
+     <a href={newsUrl} target='_blank' className="btn btn-primary mt-auto d-block" style={{ width: "fit-content" }}>Read more</a>
     </div>
    </div>
   </>
