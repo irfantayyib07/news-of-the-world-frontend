deec172f6ca37d66057c903052a7ab9f984de5fb
Irfan <irfantayyib07@gmail.com>
2024-01-25 15:08:26 +0500

    adding country falgs now

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,10 @@
 import { useFilter } from "../contexts/filterContext";
+import { COUNTRIES } from "../constants/countries";
 
 function Filter() {
  const { countryState, pageState } = useFilter();
  const [country, setCountry] = countryState;
  const [page, setPage] = pageState;
-
- const countries = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"]
 
  const handleChange = (e) => {
   setCountry(e.target.value);
@@ -14,8 +13,8 @@ function Filter() {
 
  return (
   <>
-   <select name="country" id="country" className="form-select form-select-sm w-25" aria-label="Small select example" onChange={(e) => handleChange(e)} defaultValue={country}>
-    {countries.map(country => <option value={country} key={country}>{country}</option>)}
+   <select name="country-select" id="country-select" className="form-select form-select-sm w-25" aria-label="Small select example" onChange={(e) => handleChange(e)} defaultValue={country}>
+    {COUNTRIES.map(country => <option value={country.shortName} key={country.shortName}>{country.fullName}</option>)}
    </select>
   </>
  )
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,17 +9,20 @@ import { capitalizeFirstLetter } from "../lib/utilityFunctions";
 function News({ category }) {
  const { response, loading, totalPages } = useFetchNews(category);
 
+ if (response.length === 0) return;
+
  return (
   <div className='container mt-4 position-relative'>
    <h2 className='display-6 my-4'>Top Headlines</h2>
    <div className="d-flex flex-wrap gap-3">
     <Filter />
     <p className="m-0 p-2 bg-body-tertiary border border-secondary rounded-2">Category: {capitalizeFirstLetter(category)}</p>
+    <p className="m-0 p-2 bg-body-tertiary border border-secondary rounded-2">{response.length} results found</p>
    </div>
 
    {loading ? <Spinner /> :
     <div className="row g-3 mt-4">
-     {response.length !== 0 && response.map((value) => {
+     {response.map((value) => {
       return <div className="col-sm-6 col-md-4" key={value.url}>
        <NewsItems title={value.title?.slice(0, 45)} description={value.description?.slice(0, 88)} imgUrl={value.urlToImage} newsUrl={value.url} />
       </div>
