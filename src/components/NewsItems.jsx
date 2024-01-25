e63c921197b0673173de58e69476745c4dea23b1
Irfan <irfantayyib07@gmail.com>
2024-01-25 23:36:25 +0500

    BOLD

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -6,7 +6,7 @@ function NewsItems({ title, description, imgUrl, newsUrl }) {
    <div className="card h-100">
     <img src={imgUrl || "https://www.smaroadsafety.com/wp-content/uploads/2022/06/no-pic.png"} className="card-img-top card-image" alt={"No Image Found"} />
     <div className="card-body d-flex flex-column">
-     <h5 className="card-title">{title}</h5>
+     <h5 className="card-title fw-bold">{title}</h5>
      <p className="card-text">{description}</p>
      <a href={newsUrl} target='_blank' className="btn btn-warning mt-auto d-block" style={{ width: "fit-content" }}>Read more</a>
     </div>
