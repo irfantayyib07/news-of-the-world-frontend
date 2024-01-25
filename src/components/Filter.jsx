f1e8b2794c55065baa3c6f804348e53b6b559811
Irfan <irfantayyib07@gmail.com>
2024-01-25 23:45:26 +0500

    NS

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -13,7 +13,8 @@ function Filter() {
  }
 
  return (
-  <div className="m-0 p-2 bg-body-secondary border border-secondary rounded-2 d-flex gap-2">
+  <div className="m-0 p-2 bg-body-secondary border border-secondary rounded-2 d-flex align-items-center gap-2">
+   <p className="m-0 p-0 lh-base">Presenting news from</p>
    <div className={`fi fi-${country}`}></div>
    <select name="country-select" id="country-select" className="form-select form-select-sm w-25 flex-grow-1" aria-label="Small select example" onChange={(e) => handleChange(e)} defaultValue={country}>
     {COUNTRIES.map(country => <option value={country.shortName} key={country.shortName}>{country.fullName}</option>)}
